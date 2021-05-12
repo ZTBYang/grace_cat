0e0874dc7b010ca031fe5917f9b35118938fe339
ZTBYang <2779932319@qq.com>
2021-05-12 20:33:40 +0800

    '阿巴阿巴'

--- app/Http/Controllers/Forum.php
+++ app/Http/Controllers/Forum.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 
-class forum extends Controller
+class Forum extends Controller
 {
     public function testGit() {
         return '真是小母牛站着尿，牛逼跟你开玩笑啊!';
