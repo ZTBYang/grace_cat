dfc51574f57c056fc5b2632cdf8cb590c17015c4
wangyangyang <1726467961@qq.com>
2021-05-12 17:59:35 +0800

    '哎哟我去'

---
+++ app/Http/Controllers/forum.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class forum extends Controller
+{
+    public function testGit() {
+        return '真是小母牛站着尿，牛逼跟你开玩笑啊!';
+    }
+}
