7c50561ad5dced71d41c9a5db573c8038bfa5150
wangyangyang <1726467961@qq.com>
2021-05-13 09:37:13 +0800

    '新增giegie'

--- app/Http/Controllers/forum.php
+++ app/Http/Controllers/forum.php
@@ -9,4 +9,8 @@ class forum extends Controller
     public function testGit() {
         return '真是小母牛站着尿，牛逼跟你开玩笑啊!';
     }
+
+    public function testGitHooks() {
+        return '我只心疼giegie';
+    }
 }
